0742d5bc40a7ba872a98e23c423e14025503b578
henlange <hen.lange@dspg.com>
2016-05-05 17:05:01 +0300

    aligned the tests procedure for running

--- Test_HEN_9_3_16.py
+++ Test_HEN_9_3_16.py
@@ -8,7 +8,7 @@
 ###############################
 #Hen Path File
 ###############################
-#execfile ("G:\Chip_Validation\Python\Hen\infrastructure\init.py")
+execfile ("C:\Users\henl\Documents\D6\PROJ\DBMD6\init.py")
 ###############################
 #Eran Path File
 ###############################
@@ -23,18 +23,62 @@ SerialConfig_1 (COM ,BAUD_RATE)
 
 Open_log(Log_Name)
 Sync ()
-checkSum()
-reset
-Sync () 
-checkSum()
+#checkSum()
+#reset
+#Sync () 
+#checkSum()
+
+
 read_apb_reg ("03000000")
 Clock_Out ("Global")
 System_Clock_PLL ("49")
-System_Clock_PLL ("32")
+MEM_BIST("PRAM012","103c0","030000D4","3c0")
+MEM_BIST("PRAM34","e0080","030000D4","3f")
+MEM_BIST("DTCM2","2010","030000D0","1e00")
+MEM_BIST("DTCM1","1008","030000D0","1e0")
+MEM_BIST("DTCM0","804","030000D0","1e")
+MEM_BIST("PTCM","c020","030000D0","1fe000")
+MEM_BIST("CACHE","10040","030000D4","fc00")
+MEM_BIST("HWVAD","201","030000D0","0001")
+MEM_BIST("ROM","402","0","0")
+
+#System_Clock_PLL ("32")
+
 System_Clock_PLL ("73")
+MEM_BIST("PRAM012","103c0","030000D4","3c0")
+MEM_BIST("PRAM34","e0080","030000D4","3f")
+MEM_BIST("DTCM2","2010","030000D0","1e00")
+MEM_BIST("DTCM1","1008","030000D0","1e0")
+MEM_BIST("DTCM0","804","030000D0","1e")
+MEM_BIST("PTCM","c020","030000D0","1fe000")
+MEM_BIST("CACHE","10040","030000D4","fc00")
+MEM_BIST("HWVAD","201","030000D0","0001")
+MEM_BIST("ROM","402","0","0")
+
 System_Clock_PLL ("82")
-System_Clock_PLL ("92")
+MEM_BIST("PRAM012","103c0","030000D4","3c0")
+MEM_BIST("PRAM34","e0080","030000D4","3f")
+MEM_BIST("DTCM2","2010","030000D0","1e00")
+MEM_BIST("DTCM1","1008","030000D0","1e0")
+MEM_BIST("DTCM0","804","030000D0","1e")
+MEM_BIST("PTCM","c020","030000D0","1fe000")
+MEM_BIST("CACHE","10040","030000D4","fc00")
+MEM_BIST("HWVAD","201","030000D0","0001")
+MEM_BIST("ROM","402","0","0")
+
+#System_Clock_PLL ("92")
+
 System_Clock_PLL ("98")
+MEM_BIST("PRAM012","103c0","030000D4","3c0")
+MEM_BIST("PRAM34","e0080","030000D4","3f")
+MEM_BIST("DTCM2","2010","030000D0","1e00")
+MEM_BIST("DTCM1","1008","030000D0","1e0")
+MEM_BIST("DTCM0","804","030000D0","1e")
+MEM_BIST("PTCM","c020","030000D0","1fe000")
+MEM_BIST("CACHE","10040","030000D4","fc00")
+MEM_BIST("HWVAD","201","030000D0","0001")
+MEM_BIST("ROM","402","0","0")
+
 #	time.sleep(0.1)
 #	GPIO.output(Reset_GPIO,True)
 #	time.sleep(0.1)	
@@ -54,5 +98,5 @@ System_Clock_PLL ("98")
 #MEM_BIST("PTCM","c020","030000D0","1fe000")
 #MEM_BIST("CACHE","10040","030000D4","fc00")
 #MEM_BIST("HWVAD","201","030000D0","0001")
-MEM_BIST("ROM","402","0","0")
+#MEM_BIST("ROM","402","0","0")
 
