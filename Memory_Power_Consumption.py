3b758faf3e7ead000cd2eb30b6d3b03c714a41ef
Eran Simoni <eran.simoni@dspg.com>
2016-05-15 10:40:43 +0300

    now test for the Memory_Power_Consumption that can select individual section in the memory

--- Memory_Power_Consumption.py
+++ Memory_Power_Consumption.py
@@ -26,11 +26,32 @@ Open_log(Log_Name)
 Sync ()
 checkSum()
 
-read_apb_reg ("03000000")
-Clock_Out ("Global")
+#read_apb_reg ("3000000")
+#Clock_Out ("Global")
 time.sleep(3)
-read_apb_reg ("3000084")
-
+#read_apb_reg ("3000084")
 System_Clock_PLL ("32")
+System_Clock_PLL ("98")
+#Can Select the following memory name :
+  #PTCM = 0 to 7
+  #DTCM = 0 to 9
+  #TAG = 0 to 1
+  #CACHE = 0 to 3
+  #PAHB = 0 to 9
+# shut_down , deep_sleep , light_sleep
+#All_Memory_Power_Mode ("shut_down" ,1)
+time.sleep(5)
+read_apb_reg (MEM_PWR_MD_SD1)
+Memory_Block_Select (PTCM , 7 , "shut_down")
+time.sleep(5)
+Memory_Block_Select (TAG , 1 , "shut_down")
+time.sleep(5)
+Memory_Block_Select (PAHB , 9 , "shut_down")
+time.sleep(5)
+Memory_Block_Select (CACHE , 3, "shut_down")
+time.sleep(5)
+read_apb_reg (MEM_PWR_MD_SD1)
+
+
 
 
